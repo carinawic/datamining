df3c30d03e688354092bac549ef3a19f1b71d4b1
Aida Stefania Manole <aida.manole@stud.acs.upb.ro>
2021-05-03 11:49:43 +0200

    networkit: compute some graph stats: diameter, clustering coefficient, etc.

--- networkit_processing.py
+++ networkit_processing.py
@@ -7,6 +7,34 @@ import argparse
 import powerlaw
 
 
+def compute_diameter(G):
+    # Extract the largest connected component, otherwise the resulting diameter will be infinite
+    G = nk.graphtools.toUndirected(G)
+    newGraph = nk.components.ConnectedComponents.extractLargestConnectedComponent(G, True)
+    no_nodes_lcc = newGraph.numberOfNodes()
+    print(f"No of nodes LCC {no_nodes_lcc}")
+    # Initialize algorithm to compute the exact diameter of the input graph
+    diam = nk.distance.Diameter(newGraph,algo=1)
+    diam.run()
+    # The return value of getDiameter is a pair of integers, the lower and upper bound of the
+    # diameter. In this case, the diameter is the first value of the pair
+    print(f"Graph diameter {diam.getDiameter()}")
+
+def clustering(G):
+    # The clustering coefficient is computed as the average of the local clustering
+    # coefficient over all nodes. The local clustering coefficient focuses on a single
+    # node and counts how many possible edges between neighbors of the node exist
+    cc = nk.globals.clustering(G)
+    print(f"Clustering coefficent {cc}")
+
+# the networkit implementation is based on parallel power iteration
+def eigenvector_centrality(G):
+    ec = nk.centrality.EigenvectorCentrality(G)
+    ec.run()
+    top_10 = ec.ranking()[:10] # the 10 most central nodes
+    print(top_10)
+
+
 def degree_distribution(G):
     dd = sorted(nk.centrality.DegreeCentrality(G).run().scores(), reverse=True)
     plt.xscale("log")
@@ -17,6 +45,7 @@ def degree_distribution(G):
     plt.show()
 
     fit = powerlaw.Fit(dd)
+    # Typically the exponent falls in the range 2 < alpha < 3
     print(f"Powerlaw degree distribution {fit.alpha}")
 
 
@@ -31,7 +60,10 @@ def analyse_connectivity(G):
 
 def compute_network_stats(G):
     # analyse_connectivity(G)
-    degree_distribution(G)
+    # degree_distribution(G)
+    # eigenvector_centrality(G)
+    # clustering(G)
+    compute_diameter(G)
 
 
 def load_data_gml(filepath):
@@ -39,7 +71,7 @@ def load_data_gml(filepath):
     nodes = G.numberOfNodes()
     edges = G.numberOfEdges()
     
-    print("No of nodes = %d\n No of edges = %d"%(nodes, edges))
+    print("No of nodes = %d\nNo of edges = %d"%(nodes, edges))
     return G
 
 
