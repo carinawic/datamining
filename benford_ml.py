70e6e65257c43571e3165ebd622f5f779030be80
Carina Wickström <49478094+carinawic@users.noreply.github.com>
2021-05-12 17:22:36 +0200

    change ml to consider 3 inputs

--- benford_ml.py
+++ benford_ml.py
@@ -3,54 +3,53 @@ from keras.models import Sequential
 from keras.layers.core import Dense
 import json
 
-benford_degrees = []
-degrees = []
-clusterings = []
+
 labels = []
+combined_data = []
 
 with open("benford_input.txt", encoding="utf-8") as f:
     
-    
     json_data = json.load(f)
     for i in json_data["users"]:
-        benford_degrees.append(i["benford_degree"])
-        degrees.append(i["degree"])
-        clusterings.append(i["clustering"])
         labels.append(i["real"])
+        combined_data.append([i["benford_degree"], i["degree"], i["clustering"]])
 
-training_data = benford_degrees[:12]
-training_labels = labels[:12]
-test_data = benford_degrees[12:]
-test_labels = labels[12:]
 
-print("training data:")
-print(training_data)
-print(training_labels)
-print("test data:")
-print(test_data)
-print(test_labels)
+combined_training_data = combined_data[:12]
+combined_training_labels = labels[:12]
+combined_test_data = combined_data[12:]
+combined_test_labels = labels[12:]
 
+
+# keras.Sequential groups a linear stack of layers into a Model
 model = Sequential()
-model.add(Dense(16, activation='relu'))
+# dense layer: we narrow all our nodes into 16 nodes
+# ReLU (Rectified Linear Unit) = f(x) = 0 until a certain x-value thereafter linear 
+model.add(Dense(16, activation='relu', input_dim=3))
+# last layer is 1 neuron with a value from a sigmoid function 
 model.add(Dense(1, activation='sigmoid'))
 
-
+# creating the model
 model.compile(loss='mean_squared_error',
               optimizer='adam',
               metrics=['accuracy'])
 
-model.fit(training_data, training_labels, epochs=500, verbose=2)
+# we try to fit our model to our training data
+# epochs is the amount of training rounds
+# verbose alters the terminal output type
+model.fit(combined_training_data, combined_training_labels, epochs=500, verbose=2)
 
 print("evaluating test data:")
 
-test_results = model.evaluate(test_data, test_labels)
+# testing our network on the test data
+test_results = model.evaluate(combined_test_data, combined_test_labels)
 print(test_results)
 
+'''
 # printing a summary of the model
-#model.summary()
+model.summary()
 
 # making a prediction for a specific value:
-'''
 predict = model.predict(test_data[0])
 print("prediction:", predict[0])
 print("actual:", test_labels[0])
